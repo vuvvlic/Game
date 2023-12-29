272d165e50f94dd524e2f5f2b1d16a7f0a60d2fa
vuvvlic <123775560+vuvvlic@users.noreply.github.com>
2023-12-29 18:10:12 +0300

    one

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ import pygame
 
 if __name__ == '__main__':
     pygame.init()
-    size = width, height = 800, 600
+    size = width, height = 900, 700
     screen = pygame.display.set_mode(size)
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print(event.pos)
+        picture1 = pygame.image.load("data/pole_one.png")
+        picture2 = pygame.image.load("data/pole_two.png")
+        screen.blit(picture1, (155, 50))
+        screen.blit(picture2, (413, 55))
         pygame.display.flip()
     pygame.quit()
