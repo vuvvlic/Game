ceaf7b4ed94ff4be340d994b9a6eb57c3caa564b
vuvvlic <123775560+vuvvlic@users.noreply.github.com>
2024-01-04 17:22:06 +0300

    two

--- menu.py
+++ menu.py
@@ -37,23 +37,46 @@ def start_window():
     button1 = pygame.Rect(50, 50, 200, 50)
     button2 = pygame.Rect(50, 150, 200, 50)
     button3 = pygame.Rect(50, 250, 200, 50)
+    back = pygame.image.load('data/back.png')
     running = True
     flag = False
+    flag_end = False
+    itog_total = 0
+    necessary_total = 0
+
     while running:
         if flag:
-            screen.fill(fon_color)
-            level_1 = pygame.image.load('levels/level1.png')
-            level_2 = pygame.image.load('levels/level2.png')
-            level_3 = pygame.image.load('levels/level3.png')
-            level_4 = pygame.image.load('levels/level4.png')
-            level_5 = pygame.image.load('levels/level5.png')
-            back = pygame.image.load('data/back.png')
-            screen.blit(back, (225, 400))
-            screen.blit(level_1, (50, 100))
-            screen.blit(level_2, (250, 100))
-            screen.blit(level_3, (450, 100))
-            screen.blit(level_4, (150, 200))
-            screen.blit(level_5, (350, 200))
+            if flag_end:
+                if itog_total >= necessary_total:
+                    word = f"Вы набрали нужное кол-во очков. {itog_total} из {necessary_total}"
+                    screen.fill(fon_color)
+                    winner = pygame.font.Font(None, 35)
+                    winner__ = winner.render(word, True, (0, 0, 0))
+                    screen.blit(winner__, (10, 50))
+                    sl = pygame.font.Font(None, 60).render('Победа!!!', True, (0, 0, 0))
+                    screen.blit(sl, (200, 110))
+                else:
+                    word = f"Вы не набрали нужное кол-во очков. {itog_total} из {necessary_total}"
+                    screen.fill(fon_color)
+                    winner = pygame.font.Font(None, 35)
+                    winner__ = winner.render(word, True, (0, 0, 0))
+                    screen.blit(winner__, (10, 50))
+                    sl = pygame.font.Font(None, 60).render('Проигрыш', True, (0, 0, 0))
+                    screen.blit(sl, (200, 110))
+                screen.blit(back, (225, 400))
+            else:
+                screen.fill(fon_color)
+                level_1 = pygame.image.load('levels/level1.png')
+                level_2 = pygame.image.load('levels/level2.png')
+                level_3 = pygame.image.load('levels/level3.png')
+                level_4 = pygame.image.load('levels/level4.png')
+                level_5 = pygame.image.load('levels/level5.png')
+                screen.blit(back, (225, 400))
+                screen.blit(level_1, (50, 100))
+                screen.blit(level_2, (250, 100))
+                screen.blit(level_3, (450, 100))
+                screen.blit(level_4, (150, 200))
+                screen.blit(level_5, (350, 200))
 
         else:
             screen.fill((150, 70, 0))
@@ -80,23 +103,37 @@ def start_window():
                     pass
                 elif button3.collidepoint(mousepos):
                     pass
-            if event.type == pygame.MOUSEBUTTONDOWN and flag:
+            if event.type == pygame.MOUSEBUTTONDOWN and flag and not flag_end:
                 level = ChekingTheCoordinates(event.pos[0], event.pos[1])
                 button = level.check()
                 if button == 1:
-                    playing_vuvvlic()
+                    itog_total = playing_vuvvlic()
+                    necessary_total = 70
+                    flag_end = True
                 if button == 2:
-                    playing_vuvvlic()
+                    itog_total = playing_vuvvlic()
+                    necessary_total = 100
+                    flag_end = True
                 if button == 3:
-                    playing_vuvvlic()
+                    itog_total = playing_vuvvlic()
+                    necessary_total = 150
+                    flag_end = True
                 if button == 4:
-                    playing_vuvvlic()
+                    itog_total = playing_vuvvlic()
+                    necessary_total = 200
+                    flag_end = True
                 if button == 5:
-                    playing_vuvvlic()
+                    itog_total = playing_vuvvlic()
+                    necessary_total = 250
+                    flag_end = True
                 if button == 6:
                     flag = False
                 screen = pygame.display.set_mode(size)
+            if event.type == pygame.MOUSEBUTTONDOWN and flag and flag_end:
+                level = ChekingTheCoordinates(event.pos[0], event.pos[1])
+                button = level.check()
+                if button == 6:
+                    flag_end = False
 
         pygame.display.update()
         pygame.display.flip()
-
